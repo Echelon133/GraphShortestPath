c614bbea73ea612bc2cca55ca5dc7fb189d5f7e3
Mateusz Bednarski <echelon133@gmail.com>
2019-01-28 17:02:15 +0100

    Make Edge class constructor throw IllegalArgumentException when given weight is negative

--- src/main/java/ml/echelon133/graph/Edge.java
+++ src/main/java/ml/echelon133/graph/Edge.java
@@ -6,7 +6,13 @@ public class Edge<T extends Number & Comparable<T>> {
     private Vertex<T> destination;
     private T weight;
 
-    public Edge(Vertex<T> source, Vertex<T> destination, T weight) {
+    public Edge(Vertex<T> source, Vertex<T> destination, T weight) throws IllegalArgumentException {
+
+        // For now converting every T to double to check for negative value works fine
+        if (weight.doubleValue() < 0) {
+            throw new IllegalArgumentException("Edge weight cannot be negative");
+        }
+
         this.source = source;
         this.destination = destination;
         this.weight = weight;
