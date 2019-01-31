cd72bf6663c760cac700a01d38caff9f5df35ed1
Mateusz Bednarski <echelon133@gmail.com>
2019-01-31 16:52:02 +0100

    Add initial version of ShortestPathSolver class (without any methods implemented)

---
+++ src/main/java/ml/echelon133/graph/ShortestPathSolver.java
@@ -0,0 +1,30 @@
+package ml.echelon133.graph;
+
+import java.util.*;
+
+public class ShortestPathSolver<T extends Number & Comparable<T>> {
+
+    private class VertexComparator implements Comparator<Vertex<T>> {
+
+        @Override
+        public int compare(Vertex<T> o1, Vertex<T> o2) {
+            // Comparator needs to always sort Vertex<T> elements in the PriorityQueue
+            // based on their sumOfWeights that is stored in VertexResult (value of resultMap)
+            T o1SumOfWeights = resultMap.get(o1).getSumOfWeights();
+            T o2SumOfWeights = resultMap.get(o2).getSumOfWeights();
+            return o1SumOfWeights.compareTo(o2SumOfWeights);
+        }
+    }
+
+    private Graph<T> graph;
+    private Set<Vertex<T>> visitedVertexes;
+    private PriorityQueue<Vertex<T>> workQueue;
+    private Map<Vertex<T>, VertexResult<T>> resultMap;
+
+    public ShortestPathSolver(Graph<T> graph) {
+        this.graph = graph;
+        this.visitedVertexes = new HashSet<>();
+        this.workQueue = new PriorityQueue<>(new VertexComparator());
+        this.resultMap = new HashMap<>();
+    }
+}
