9c4f1ae4b886e093d0ddab3e6caa16aaf673ff07
Mateusz Bednarski <echelon133@gmail.com>
2019-02-04 16:08:05 +0100

    Update TestGraphStore with a test Graph<Integer> example

--- src/test/java/ml/echelon133/graph/TestGraphStore.java
+++ src/test/java/ml/echelon133/graph/TestGraphStore.java
@@ -76,8 +76,41 @@ public class TestGraphStore {
         return graph;
     }
 
+    /*
+        If we start from intVertex1, expected solution is:
+
+        intVertex1 | Prev vertex: ---        | sumOfWeights to intVertex1: 0
+        intVertex2 | Prev vertex: intVertex5 | sumOfWeights to intVertex2: 2147485677
+        intVertex3 | Prev vertex: intVertex1 | sumOfWeights to intVertex3: 1000
+        intVertex4 | Prev vertex: intVertex3 | sumOfWeights to intVertex4: 1200
+        intVertex5 | Prev vertex: intVertex6 | sumOfWeights to intVertex5: 2147485647
+        intVertex6 | Prev vertex: intVertex3 | sumOfWeights to intVertex6: 2000
+
+         */
     public static Graph<Integer> getIntegerTestGraph() {
-        return new WeightedGraph<>();
+        Graph<Integer> graph = new WeightedGraph<>();
+
+        Vertex<Integer> v1 = new Vertex<>("intVertex1");
+        Vertex<Integer> v2 = new Vertex<>("intVertex2");
+        Vertex<Integer> v3 = new Vertex<>("intVertex3");
+        Vertex<Integer> v4 = new Vertex<>("intVertex4");
+        Vertex<Integer> v5 = new Vertex<>("intVertex5");
+        Vertex<Integer> v6 = new Vertex<>("intVertex6");
+
+        List<Vertex<Integer>> allVertexes = List.of(v1, v2, v3, v4, v5, v6);
+        allVertexes.forEach(graph::addVertex);
+
+        graph.addEdge(v1, v3, 1000);
+        graph.addEdge(v3, v6, 1000);
+        graph.addEdge(v3, v4, 200);
+        graph.addEdge(v4, v3, 800);
+        graph.addEdge(v6, v5, Integer.MAX_VALUE);
+        graph.addEdge(v5, v2, 30);
+        graph.addEdge(v2, v4, 15);
+        graph.addEdge(v2, v1, 80);
+        graph.addEdge(v4, v1, 30);
+
+        return graph;
     }
 
     public static Graph<Long> getLongTestGraph() {
