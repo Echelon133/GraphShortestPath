43cf10459625f3f21c918b86fb689b8ff5860eb5
Mateusz Bednarski <echelon133@gmail.com>
2019-02-04 15:02:58 +0100

    Update TestGraphStore with a test Graph<Short> example

--- src/test/java/ml/echelon133/graph/TestGraphStore.java
+++ src/test/java/ml/echelon133/graph/TestGraphStore.java
@@ -42,8 +42,38 @@ public class TestGraphStore {
         return graph;
     }
 
+    /*
+    If we start from shortVertex1, expected solution is:
+
+    shortVertex1 | Prev vertex: ---          | sumOfWeights to shortVertex1: 0
+    shortVertex2 | Prev vertex: shortVertex5 | sumOfWeights to shortVertex2: 9
+    shortVertex3 | Prev vertex: shortVertex5 | sumOfWeights to shortVertex3: 5
+    shortVertex4 | Prev vertex: shortVertex2 | sumOfWeights to shortVertex4: 32772
+    shortVertex5 | Prev vertex: shortVertex1 | sumOfWeights to shortVertex5: 32767
+
+     */
     public static Graph<Short> getShortTestGraph() {
-        return new WeightedGraph<>();
+        Graph<Short> graph = new WeightedGraph<>();
+
+        Vertex<Short> v1 = new Vertex<>("shortVertex1");
+        Vertex<Short> v2 = new Vertex<>("shortVertex2");
+        Vertex<Short> v3 = new Vertex<>("shortVertex3");
+        Vertex<Short> v4 = new Vertex<>("shortVertex4");
+        Vertex<Short> v5 = new Vertex<>("shortVertex5");
+
+        List<Vertex<Short>> allVertexes = List.of(v1, v2, v3, v4, v5);
+        allVertexes.forEach(graph::addVertex);
+
+        graph.addEdge(v1, v2, (short)10);
+        graph.addEdge(v1, v2, (short)12);
+        graph.addEdge(v1, v5, Short.MAX_VALUE);
+        graph.addEdge(v1, v3, (short)5);
+        graph.addEdge(v3, v4, Short.MAX_VALUE);
+        graph.addEdge(v4, v3, (short)1);
+        graph.addEdge(v4, v5, (short)18);
+        graph.addEdge(v3, v2, (short)4);
+
+        return graph;
     }
 
     public static Graph<Integer> getIntegerTestGraph() {
