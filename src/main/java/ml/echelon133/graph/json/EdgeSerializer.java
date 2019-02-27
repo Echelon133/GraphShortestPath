3a5d9524c42643f7f6928154abd85ac94f2b2219
Mateusz Bednarski <echelon133@gmail.com>
2019-02-27 15:39:11 +0100

    Add EdgeSerializer

---
+++ src/main/java/ml/echelon133/graph/json/EdgeSerializer.java
@@ -0,0 +1,29 @@
+package ml.echelon133.graph.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.ser.std.StdSerializer;
+import ml.echelon133.graph.Edge;
+
+import java.io.IOException;
+
+public class EdgeSerializer extends StdSerializer<Edge<?>> {
+
+    public EdgeSerializer(JavaType type) {
+        super(type);
+    }
+
+    @Override
+    public void serialize(Edge<?> value, JsonGenerator gen, SerializerProvider provider) throws IOException {
+        gen.writeStartObject();
+        gen.writeStringField("source", value.getSource().getName());
+        gen.writeStringField("destination", value.getDestination().getName());
+
+        // serialize graph weight using BigDecimal representation
+        // serializing actual weight with additional field that stores info about the type makes deserialization difficult
+        // preserving the actual type does not seem crucial after considering the complexity of deserialization process
+        gen.writeNumberField("weight", value.getWeightAsBigDecimal());
+        gen.writeEndObject();
+    }
+}
