8761cfb9ec77a5445ca66049a936819eb94296f6
Mateusz Bednarski <echelon133@gmail.com>
2019-03-01 17:58:34 +0100

    Change NodeIsNotNumberException

--- src/main/java/ml/echelon133/graph/json/exception/NodeIsNotNumberException.java
+++ src/main/java/ml/echelon133/graph/json/exception/NodeIsNotNumberException.java
@@ -2,9 +2,9 @@ package ml.echelon133.graph.json.exception;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 
-public class NodeIsNotBigDecimalException extends JsonProcessingException {
+public class NodeIsNotNumberException extends JsonProcessingException {
 
-    public NodeIsNotBigDecimalException(String msg) {
+    public NodeIsNotNumberException(String msg) {
         super(msg);
     }
 }
